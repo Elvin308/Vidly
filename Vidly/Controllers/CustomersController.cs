80686a7e9bfcce166219115f56a333969b431c1d
Elvin Martinez <43117188+Elvin308@users.noreply.github.com>
2024-12-09 22:44:24 -0600

    Initial validation to new/edit customer page

--- Vidly/Controllers/CustomersController.cs
+++ Vidly/Controllers/CustomersController.cs
@@ -42,6 +42,17 @@ namespace Vidly.Controllers
         [HttpPost]
         public ActionResult Save(Customer customer)
         {
+            if (!ModelState.IsValid) //Checkl if the customer class rules are being kept
+            {
+                var viewModel = new CustomerFormViewModel
+                {
+                    Customer = customer,
+                    MembershipTypes = _context.MembershipTypes.ToList()
+                };
+
+                return View("CustomerForm",viewModel);
+            }
+
             if (customer.Id == 0)
             {
                 _context.Customers.Add(customer);
